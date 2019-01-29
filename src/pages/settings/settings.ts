4e44890a5eaa8ad73785d498fab26ed3dd6f55f8
john.raftery <john.raftery@accenture.com>
2019-01-29 17:31:06 +0000

    remove notifications from master

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController, Events, IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Storage } from "@ionic/storage";
-import { LocalNotifications } from "@ionic-native/local-notifications";
 
 /**
  * Generated class for the SettingsPage page.
@@ -23,8 +22,7 @@ export class SettingsPage {
               public navParams: NavParams,
               public events: Events,
               private storage: Storage,
-              public alertCtrl: AlertController,
-              private localNotifications: LocalNotifications) {
+              public alertCtrl: AlertController) {
     this.settings = {};
     this.storage.get("stored_settings").then((settings) => {
       this.settings = settings;
@@ -49,31 +47,5 @@ export class SettingsPage {
     this.storage.set("stored_settings", this.settings).then( () => {
       this.events.publish('passagesChanged');
     });
-  }
-
-  changeNotification() {
-    this.storage.set("stored_settings", this.settings);
-    if (!this.settings.reminderNotification) {
-      this.localNotifications.clearAll();
-    }
-    else if (this.settings.reminderNotificationTime) {
-      this.setNotification();
-    }
-  }
-
-  changeNotificationTime() {
-    this.storage.set("stored_settings", this.settings);
-    this.setNotification();
-  }
-
-  setNotification() {
-    this.localNotifications.clearAll();
-    var timeSplit = this.settings.reminderNotificationTime.split(":");
-    let notification = {
-      id: 1,
-      text: 'Let the word of Christ dwell in you richly',
-      trigger: { every: { hour: parseInt(timeSplit[0]), minute: parseInt(timeSplit[1]) }, count: 1 }
-    };
-    this.localNotifications.schedule(notification);
   }
 }
