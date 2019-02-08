3de2138ba32f20b08a7f3ce78209435ad31ca04c
James Doc <jamesdoc@gmail.com>
2019-02-08 14:54:58 +0100

    Seperate data from controller

--- src/pages/add-passage/add-passage.ts
+++ src/pages/add-passage/add-passage.ts
@@ -6,6 +6,7 @@ import { SocialSharing } from "@ionic-native/social-sharing";
 import { Events } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import moment from 'moment';
+import { bookChapters } from './bookChapters';
 
 /**
  * Generated class for the AddPassagePage page.
@@ -32,74 +33,7 @@ export class AddPassagePage {
   chapter = "";
   startVerse = "";
   endVerse = "";
-  bookChapters = {
-    "Genesis": 50,
-    "Exodus": 40,
-    "Leviticus": 27,
-    "Numbers": 36,
-    "Deuteronomy": 34,
-    "Joshua": 24,
-    "Judges": 21,
-    "Ruth": 4,
-    "1 Samuel": 31,
-    "2 Samuel": 24,
-    "1 Kings": 22,
-    "2 Kings": 25,
-    "1 Chronicles": 29,
-    "2 Chronicles": 36,
-    "Ezra": 10,
-    "Nehemiah": 13,
-    "Esther": 10,
-    "Job": 42,
-    "Psalm": 150,
-    "Proverbs": 31,
-    "Ecclesiastes": 12,
-    "Song of Solomon": 8,
-    "Isaiah": 66,
-    "Jeremiah": 52,
-    "Lamentations": 5,
-    "Ezekiel": 48,
-    "Daniel": 12,
-    "Hosea": 14,
-    "Joel": 3,
-    "Amos": 9,
-    "Obadiah": 1,
-    "Jonah": 4,
-    "Micah": 7,
-    "Nahum": 3,
-    "Habakkuk": 3,
-    "Zephaniah": 3,
-    "Haggai": 2,
-    "Zechariah": 14,
-    "Malachi": 4,
-    "Matthew": 28,
-    "Mark": 16,
-    "Luke": 24,
-    "John": 21,
-    "Acts": 28,
-    "Romans": 16,
-    "1 Corinthians": 16,
-    "2 Corinthians": 13,
-    "Galatians": 6,
-    "Ephesians": 6,
-    "Philippians": 4,
-    "Colossians": 4,
-    "1 Thessalonians": 5,
-    "2 Thessalonians": 3,
-    "1 Timothy": 6,
-    "2 Timothy": 4,
-    "Titus": 3,
-    "Philemon": 1,
-    "Hebrews": 13,
-    "James": 5,
-    "1 Peter": 5,
-    "2 Peter": 3,
-    "1 John": 5,
-    "2 John": 1,
-    "3 John": 1,
-    "Jude": 1,
-    "Revelation": 21
-  };
+  bookChapters = bookChapters;
   objectKeys = Object.keys;
 
   constructor(public navCtrl: NavController,
@@ -153,6 +87,7 @@ export class AddPassagePage {
     //   toast.present();
     // }
     // else
+
     if (!this.book) {
       let toast = this.toastCtrl.create({
         message: 'Please select a book',
@@ -169,9 +104,9 @@ export class AddPassagePage {
       });
       toast.present();
     }
-    else if (parseInt(this.chapter) > this.bookChapters[this.book]) {
+    else if (parseInt(this.chapter) > bookChapters[this.book]) {
       let toast = this.toastCtrl.create({
-        message: 'There are only ' + this.bookChapters[this.book] + ' chapters in ' + this.book,
+        message: 'There are only ' + bookChapters[this.book] + ' chapters in ' + this.book,
         duration: 2000,
         position: 'bottom'
       });
